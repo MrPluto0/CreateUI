4755a10dd8b6474e8eeae533c97797dbd4f8b85a
Gypsophlia <710852553@qq.com>
2022-03-19 01:33:30 +0800

    update

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# git push -f将本地的代码库推送到远端，并覆盖
+git push -f https://github.com/MrPluto0/CreateUI.git gh-pages
